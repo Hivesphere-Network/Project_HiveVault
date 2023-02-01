6a53ea7a227ddc8e8f0257d250e696ae99f8d395
Pramuditha Pothuwila <pramudithapothuwila36@gmail.com>
2023-02-01 13:01:47 +0530

    MindRepository renamed to MindRepository_API

--- MindDepository_API/API/GraphDbApi.cs
+++ MindDepository_API/API/GraphDbApi.cs
@@ -9,6 +9,9 @@ public static class GraphDbApi
 
     private static  async Task<IResult> TestConnection()
     {
+        //var result = await session.RunAsync("MATCH p=()-[:ACTED_IN]->() RETURN p LIMIT 25;");
         return Results.Ok();
     }
+    
+    
 }
