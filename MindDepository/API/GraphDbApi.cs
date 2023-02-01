d689d15501272d1f22a0573b56b83ac4498d459d
Pramuditha Pothuwila <pramudithapothuwila36@gmail.com>
2023-02-01 11:29:55 +0530

    DatabaseHandler.cs Added GraphDbApi.cs Added Program.cs cleaned up and app.ConfigureApi() added

---
+++ MindDepository/API/GraphDbApi.cs
@@ -0,0 +1,14 @@
+﻿namespace MindDepository.API;
+
+public static class GraphDbApi
+{
+    public static void ConfigureApi(this WebApplication app)
+    {
+        app.MapGet("/api/graph/test", TestConnection).WithName("GraphConnectionTest");
+    }
+
+    private static  async Task<IResult> TestConnection()
+    {
+        return Results.Ok();
+    }
+}
---
+++ MindDepository/DB/DatabaseHandler.cs
@@ -0,0 +1,33 @@
+﻿using Neo4j.Driver;
+
+namespace MindDepository.DB;
+
+public class DatabaseHandler
+{
+    private IAsyncSession _graphSession;
+    
+    public DatabaseHandler()
+    {
+        ConnectNeo4j();
+    }
+    
+    public GetSession()
+    {
+        return _graphSession;
+    }
+    
+    private static void ConnectNeo4j()
+    {
+        try
+        {
+            var driver = GraphDatabase.Driver("b2369ae1.databases.neo4j.io:7687",
+                AuthTokens.Basic("neo4j", "k4cXvrOkYZgBELLTUsWS-yR2c5WGAqF4rIu-BbXwe6w"));
+            _graphSession = driver.AsyncSession();
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e);
+            throw;
+        }
+    }
+}
