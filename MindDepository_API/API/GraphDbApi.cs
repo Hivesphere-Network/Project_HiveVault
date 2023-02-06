7f7f6389534398270c11f4fc930e990069b3e6b4
Restyled.io <commits@restyled.io>
2023-02-06 15:21:01 +0000

    Restyled by clang-format

--- MindDepository_API/API/GraphDbApi.cs
+++ MindDepository_API/API/GraphDbApi.cs
@@ -1,14 +1,13 @@
 ﻿namespace MindDepository_API.API;
-public static class GraphDbApi
-{
-    public static void ConfigureApi(this WebApplication app)
-    {
-        app.MapGet("/api/graph/test", TestConnection).WithName("GraphConnectionTest");
-    }
+public static class GraphDbApi {
+  public static void ConfigureApi(this WebApplication app) {
+    app.MapGet("/api/graph/test", TestConnection)
+        .WithName("GraphConnectionTest");
+  }
 
-    private static async Task<IResult> TestConnection()
-    {
-        //var result = await session.RunAsync("MATCH p=()-[:ACTED_IN]->() RETURN p LIMIT 25;");
-        return Results.Ok();
-    }
+  private static async Task<IResult> TestConnection() {
+    // var result = await session.RunAsync("MATCH p=()-[:ACTED_IN]->() RETURN p
+    // LIMIT 25;");
+    return Results.Ok();
+  }
 }
