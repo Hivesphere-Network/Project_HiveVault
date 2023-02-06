de98b424997e57b79469e461f6bc59ecabab4e1d
Restyled.io <commits@restyled.io>
2023-02-06 15:36:06 +0000

    Restyled by clang-format

--- MindDepository_API/Program.cs
+++ MindDepository_API/Program.cs
@@ -8,10 +8,9 @@ builder.Services.AddSwaggerGen();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
+if (app.Environment.IsDevelopment()) {
+  app.UseSwagger();
+  app.UseSwaggerUI();
 }
 
 app.ConfigureApi();
