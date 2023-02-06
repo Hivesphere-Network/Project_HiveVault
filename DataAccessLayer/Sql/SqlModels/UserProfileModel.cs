350f24788b3a0060c9b5e7b558cf77908473151f
Restyled.io <commits@restyled.io>
2023-02-06 16:07:34 +0000

    Restyled by clang-format

--- DataAccessLayer/Sql/SqlModels/UserProfileModel.cs
+++ DataAccessLayer/Sql/SqlModels/UserProfileModel.cs
@@ -6,30 +6,11 @@ using System.Threading.Tasks;
 
 namespace DataAccessLayer.Sql.SqlModels;
 
-public class UserProfileModel
-{
-    public int Id {
-        get;
-        set;
-    }
-    public string FirstName {
-        get;
-        set;
-    }
-    public string LastName {
-        get;
-        set;
-    }
-    public string Email {
-        get;
-        set;
-    }
-    public string AccessLevel {
-        get;
-        set;
-    }
-    public string Avatar {
-        get;
-        set;
-    }
+public class UserProfileModel {
+  public int Id { get; set; }
+  public string FirstName { get; set; }
+  public string LastName { get; set; }
+  public string Email { get; set; }
+  public string AccessLevel { get; set; }
+  public string Avatar { get; set; }
 }
