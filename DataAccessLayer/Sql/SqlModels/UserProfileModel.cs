651c7caeeddfa827c23ea430db9a65c762e33620
Restyled.io <commits@restyled.io>
2023-02-06 16:07:31 +0000

    Restyled by astyle

--- DataAccessLayer/Sql/SqlModels/UserProfileModel.cs
+++ DataAccessLayer/Sql/SqlModels/UserProfileModel.cs
@@ -8,10 +8,28 @@ namespace DataAccessLayer.Sql.SqlModels;
 
 public class UserProfileModel
 {
-    public int Id { get; set; }
-    public string FirstName { get; set; }
-    public string LastName { get; set; }
-    public string Email { get; set; }
-    public string AccessLevel { get; set; }
-    public string Avatar { get; set; }
+    public int Id {
+        get;
+        set;
+    }
+    public string FirstName {
+        get;
+        set;
+    }
+    public string LastName {
+        get;
+        set;
+    }
+    public string Email {
+        get;
+        set;
+    }
+    public string AccessLevel {
+        get;
+        set;
+    }
+    public string Avatar {
+        get;
+        set;
+    }
 }
