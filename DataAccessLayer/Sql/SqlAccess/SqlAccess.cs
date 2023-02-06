cda84eda909b1c724ad12dca41100e55bb728dba
Restyled.io <commits@restyled.io>
2023-02-06 15:38:59 +0000

    Restyled by clang-format

--- DataAccessLayer/Sql/SqlAccess/SqlAccess.cs
+++ DataAccessLayer/Sql/SqlAccess/SqlAccess.cs
@@ -5,44 +5,37 @@ using Microsoft.Data.SqlClient;
 
 namespace DataAccessLayer.Sql.SqlAccess;
 
-public class SqlDataAccess : ISqlDataAccess
-{
-    private readonly IConfiguration _config;
+public class SqlDataAccess : ISqlDataAccess {
+  private readonly IConfiguration _config;
 
-    public SqlDataAccess(IConfiguration config) {
-        _config = config;
-    }
+  public SqlDataAccess(IConfiguration config) { _config = config; }
 
-    public async Task<IEnumerable<T>>
-    LoadData<T, U>(string storedProcedure, U parameters,
-                   string connectionId = "Default")
-    {
-        using IDbConnection connection =
-            new SqlConnection(_config.GetConnectionString(connectionId));
+  public async Task<IEnumerable<T>>
+  LoadData<T, U>(string storedProcedure, U parameters,
+                 string connectionId = "Default") {
+    using IDbConnection connection =
+        new SqlConnection(_config.GetConnectionString(connectionId));
 
-        return await connection.QueryAsync<T>(
-                   storedProcedure, parameters, commandType: CommandType.StoredProcedure);
-    }
+    return await connection.QueryAsync<T>(
+        storedProcedure, parameters, commandType: CommandType.StoredProcedure);
+  }
 
-    public async Task<IEnumerable<T>> LoadDataAsync<T, TU>(
-        string storedProcedure,
-        TU parameters,
-        string connectionId = "Default")
-    {
-        using IDbConnection connection = new SqlConnection(_config.GetConnectionString(connectionId));
+  public async Task<IEnumerable<T>>
+  LoadDataAsync<T, TU>(string storedProcedure, TU parameters,
+                       string connectionId = "Default") {
+    using IDbConnection connection =
+        new SqlConnection(_config.GetConnectionString(connectionId));
 
-        return await connection.QueryAsync<T>(storedProcedure, parameters,
-                                              commandType: CommandType.StoredProcedure);
-    }
+    return await connection.QueryAsync<T>(
+        storedProcedure, parameters, commandType: CommandType.StoredProcedure);
+  }
 
-    public async Task SaveDataAsync<T>(
-        string storedProcedure,
-        T parameters,
-        string connectionId = "Default")
-    {
-        using IDbConnection connection = new SqlConnection(_config.GetConnectionString(connectionId));
+  public async Task SaveDataAsync<T>(string storedProcedure, T parameters,
+                                     string connectionId = "Default") {
+    using IDbConnection connection =
+        new SqlConnection(_config.GetConnectionString(connectionId));
 
-        await connection.ExecuteAsync(storedProcedure, parameters,
-                                      commandType: CommandType.StoredProcedure);
-    }
+    await connection.ExecuteAsync(storedProcedure, parameters,
+                                  commandType: CommandType.StoredProcedure);
+  }
 }
