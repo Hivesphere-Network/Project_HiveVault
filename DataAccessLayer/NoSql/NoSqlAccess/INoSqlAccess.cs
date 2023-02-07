0a5f13f8514402e806585b5d4f1340176fc15acf
Pramuditha Pothuwila <pramudithapothuwila36@gmail.com>
2023-02-07 20:33:54 +0530

    MongoDB Access Structures Created

---
+++ DataAccessLayer/NoSql/NoSqlAccess/INoSqlAccess.cs
@@ -0,0 +1,10 @@
+﻿namespace DataAccessLayer.NoSql.NoSqlAccess;
+
+// MongoDB Access
+
+public interface INoSqlAccess
+{
+    Task<IEnumerable<T>> LoadDataAsync<T, U>(string collectionName, U parameters);
+    Task SaveDataAsync<T>(string collectionName, T parameters);
+    
+}
---
+++ DataAccessLayer/NoSql/NoSqlAccess/NoSqlAccess.cs
@@ -0,0 +1,14 @@
+﻿namespace DataAccessLayer.NoSql.NoSqlAccess;
+
+public class NoSqlAccess : INoSqlAccess
+{
+    public Task<IEnumerable<T>> LoadDataAsync<T, U>(string collectionName, U parameters)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task SaveDataAsync<T>(string collectionName, T parameters)
+    {
+        throw new NotImplementedException();
+    }
+}
