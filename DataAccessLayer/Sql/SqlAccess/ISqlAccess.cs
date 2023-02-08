8464919f40e33319edba9ae3eaf719789119bb0a
Pramuditha Pothuwila <pramudithapothuwila36@gmail.com>
2023-02-08 10:00:58 +0530

    SqlAccess class and interface removed. UserProfileModel Removed

--- DataAccessLayer/Sql/SqlAccess/ISqlAccess.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace DataAccessLayer.Sql.SqlAccess;
-
-public interface ISqlDataAccess
-{
-    Task<IEnumerable<T>> LoadDataAsync<T, U>(string storedProcedure, U parameters, string connectionId = "Default");
-    Task SaveDataAsync<T>(string storedProcedure, T parameters, string connectionId = "Default");
-}
