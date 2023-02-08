d9c5aaeb7f53775fe1ec4736409a0d5c601d3b34
Restyled.io <commits@restyled.io>
2023-02-08 08:34:12 +0000

    Restyled by clang-format

--- DataAccessLayer/NoSql/NoSqlQueryHandler.cs
+++ DataAccessLayer/NoSql/NoSqlQueryHandler.cs
@@ -1,14 +1,12 @@
 ﻿namespace DataAccessLayer.NoSql;
 
-public class NoSqlQueryHandler : INoSqlQueryHandler
-{
-    public Task<IEnumerable<T>> LoadDataAsync<T, U>(string collectionName, U parameters)
-    {
-        throw new NotImplementedException();
-    }
+public class NoSqlQueryHandler : INoSqlQueryHandler {
+  public Task<IEnumerable<T>> LoadDataAsync<T, U>(string collectionName,
+                                                  U parameters) {
+    throw new NotImplementedException();
+  }
 
-    public Task SaveDataAsync<T>(string collectionName, T parameters)
-    {
-        throw new NotImplementedException();
-    }
+  public Task SaveDataAsync<T>(string collectionName, T parameters) {
+    throw new NotImplementedException();
+  }
 }
--- DataAccessLayer/Sql/SqlQueryHandler.cs
+++ DataAccessLayer/Sql/SqlQueryHandler.cs
@@ -5,42 +5,36 @@ using Microsoft.Extensions.Configuration;
 
 namespace DataAccessLayer.Sql;
 
-public class SqlQueryHandler : ISqlQueryHandler
-{
-    private readonly IConfiguration _config;
+public class SqlQueryHandler : ISqlQueryHandler {
+  private readonly IConfiguration _config;
 
-    public SqlQueryHandler(IConfiguration _config)
-    {
-        this._config = _config;
-    }
+  public SqlQueryHandler(IConfiguration _config) { this._config = _config; }
 
-    public Task ExecuteAsync<T>(string query, T parameters, string connectionId = "Sql")
-    {
-        using IDbConnection dbConnection = new SqlConnection(_config.GetConnectionString(connectionId));
+  public Task ExecuteAsync<T>(string query, T parameters,
+                              string connectionId = "Sql") {
+    using IDbConnection dbConnection =
+        new SqlConnection(_config.GetConnectionString(connectionId));
 
-        var Result = dbConnection.QueryAsync(query, parameters);
-        return Result;
-    }
+    var Result = dbConnection.QueryAsync(query, parameters);
+    return Result;
+  }
 
-    public async Task<IEnumerable<T>> LoadDataAsync<T, TU>(
-        string storedProcedure,
-        TU parameters,
-        string connectionId = "Default")
-    {
-        using IDbConnection connection = new SqlConnection(_config.GetConnectionString(connectionId));
+  public async Task<IEnumerable<T>>
+  LoadDataAsync<T, TU>(string storedProcedure, TU parameters,
+                       string connectionId = "Default") {
+    using IDbConnection connection =
+        new SqlConnection(_config.GetConnectionString(connectionId));
 
-        return await connection.QueryAsync<T>(storedProcedure, parameters,
-                                              commandType: CommandType.StoredProcedure);
-    }
+    return await connection.QueryAsync<T>(
+        storedProcedure, parameters, commandType: CommandType.StoredProcedure);
+  }
 
-    public async Task SaveDataAsync<T>(
-        string storedProcedure,
-        T parameters,
-        string connectionId = "Default")
-    {
-        using IDbConnection connection = new SqlConnection(_config.GetConnectionString(connectionId));
+  public async Task SaveDataAsync<T>(string storedProcedure, T parameters,
+                                     string connectionId = "Default") {
+    using IDbConnection connection =
+        new SqlConnection(_config.GetConnectionString(connectionId));
 
-        await connection.ExecuteAsync(storedProcedure, parameters,
-                                      commandType: CommandType.StoredProcedure);
-    }
+    await connection.ExecuteAsync(storedProcedure, parameters,
+                                  commandType: CommandType.StoredProcedure);
+  }
 }
