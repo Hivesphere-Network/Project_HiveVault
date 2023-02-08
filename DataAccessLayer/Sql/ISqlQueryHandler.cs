e49b8d0c3c4b2b83e1f956ae766c35a8786b45b5
Restyled.io <commits@restyled.io>
2023-02-08 08:36:26 +0000

    Restyled by clang-format

--- DataAccessLayer/Sql/ISqlQueryHandler.cs
+++ DataAccessLayer/Sql/ISqlQueryHandler.cs
@@ -1,11 +1,10 @@
 ﻿namespace DataAccessLayer.Sql;
 
-public interface ISqlQueryHandler
-{
-    Task ExecuteAsync<T>(string query, T parameters,
-                         string connectionId = "Default");
-    Task<IEnumerable<T>> LoadDataAsync<T, U>(string storedProcedure, U parameters,
-            string connectionId = "Default");
-    Task SaveDataAsync<T>(string storedProcedure, T parameters,
-                          string connectionId = "Default");
+public interface ISqlQueryHandler {
+  Task ExecuteAsync<T>(string query, T parameters,
+                       string connectionId = "Default");
+  Task<IEnumerable<T>> LoadDataAsync<T, U>(string storedProcedure, U parameters,
+                                           string connectionId = "Default");
+  Task SaveDataAsync<T>(string storedProcedure, T parameters,
+                        string connectionId = "Default");
 }
