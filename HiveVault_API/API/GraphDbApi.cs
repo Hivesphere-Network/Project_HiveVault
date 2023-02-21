c96a86f5e2b4a855ea02cacf2a4274efd6eb1185
Pramuditha Pothuwila <pramudithapothuwila36@gmail.com>
2023-02-21 10:49:40 +0530

    Update GraphDbApi.cs

--- HiveVault_API/API/GraphDbApi.cs
+++ HiveVault_API/API/GraphDbApi.cs
@@ -1,17 +1,16 @@
-﻿namespace HiveVault_API.API
-{
-    public static class GraphDbApi
-    {
-        public static void ConfigureGraphApi(this WebApplication app)
-        {
-            app.MapGet("/api/graph/test", TestConnectionAsync).WithName("GraphConnectionTest");
-        }
+﻿namespace HiveVault_API.API;
 
-        private static async Task<IResult> TestConnectionAsync()
-        {
-            // var result = await session.RunAsync("MATCH p=()-[:ACTED_IN]->() RETURN p
-            // LIMIT 25;");
-            return Results.Ok();
-        }
+public static class GraphDbApi
+{
+    public static void ConfigureGraphApi(this WebApplication app)
+    {
+        app.MapGet("/api/graph/test", TestConnectionAsync).WithName("GraphConnectionTest");
+    }
+
+    private static async Task<IResult> TestConnectionAsync()
+    {
+        // var result = await session.RunAsync("MATCH p=()-[:ACTED_IN]->() RETURN p
+        // LIMIT 25;");
+        return Results.Ok();
     }
 }
