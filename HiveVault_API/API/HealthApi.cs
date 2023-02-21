306c581ccb1d705589a552d6a0c1b33341db466b
Pramuditha Pothuwila <pramudithapothuwila36@gmail.com>
2023-02-21 10:42:35 +0530

    Project Name updated

---
+++ HiveVault_API/API/HealthApi.cs
@@ -0,0 +1,14 @@
+﻿namespace HiveVault_API.API;
+
+public static class HealthApi
+{
+    public static void ConfigureApi(this WebApplication app)
+    {
+        app.MapGet("/api/health", HealthCheckAsync).WithName("HealthCheck");
+    }
+
+    private static async Task<IResult> HealthCheckAsync()
+    {
+        return Results.Ok();
+    }
+}
