b3c7f10f16acbbe8b62f5fb0cc686ebefd61ff7e
Pramuditha Pothuwila <pramudithapothuwila36@gmail.com>
2023-03-06 14:11:15 +0530

    HiveHandshakeService added to application

--- HiveVaultService/Program.cs
+++ HiveVaultService/Program.cs
@@ -1,11 +1,13 @@
 using DataAccess.Contracts;
 using DataAccess;
+using HiveVaultService.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Configuration["serverID"] = Guid.NewGuid().ToString();
 
 builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddGrpc();
 builder.Services.AddSingleton<ISqlQueryHandler, SqlQueryHandler>();
 builder.Services.AddSingleton<INoSqlQueryHandler, NoSqlQueryHandler>();
 builder.Services.AddSingleton<IGraphQueryHandler, GraphQueryHandler>();
@@ -15,5 +17,7 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 
 app.UseHttpsRedirection();
+
+app.MapGrpcService<HiveHandshakeService>();
 
 app.Run();
