30591385a5cca905df1f528de521ee2206c52f21
Pramuditha Pothuwila <pramudithapothuwila36@gmail.com>
2023-03-09 17:05:23 +0530

    Update HiveNoSqlDataService.cs

--- HiveVaultService/Services/HiveNoSqlDataService.cs
+++ HiveVaultService/Services/HiveNoSqlDataService.cs
@@ -8,11 +8,13 @@ namespace HiveVaultService.Services;
 public class HiveNoSqlDataService : HiveNoSqlData.HiveNoSqlDataBase
 {
     private INoSqlQueryHandler _repo = new NoSqlQueryHandler();
-    public override Task<NosqlDataResponse> GetNoSqlData(NosqlDataRequest request, ServerCallContext context)
+    public override async Task<NosqlDataResponse> GetNoSqlData(NosqlDataRequest request, ServerCallContext context)
     {
-        
-        var response = _repo.LoadDataAsync(request.CollectionName, request.SearchKeyParameter, request.SearchKey);
-        return base.GetNoSqlData(request, context);
+        var response = _repo.LoadDataAsync(request.Collection, request.Key, request.Value);
+        return await Task.FromResult(new NosqlDataResponse
+        {
+            Value = response.First().ToString()
+        });
     }
 }
 
