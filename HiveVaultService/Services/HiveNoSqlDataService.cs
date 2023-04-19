00feb366aac45c78ab229f19b4387e534ebfdf6a
Pramuditha Pothuwila <pramudithapothuwila36@gmail.com>
2023-04-19 09:44:15 +0530

    Refactor gRPC service methods to use a dictionary of search parameters for data retrieval
    New method add for large scale data retrieval

--- HiveVaultService/Services/HiveNoSqlDataService.cs
+++ HiveVaultService/Services/HiveNoSqlDataService.cs
@@ -9,15 +9,25 @@ namespace HiveVaultService.Services;
 public class HiveNoSqlDataService : HiveNoSqlData.HiveNoSqlDataBase
 {
     private readonly INoSqlQueryHandler _repo = new NoSqlQueryHandler();
-    public override async Task<SingleDataReadResponse> GetSingleData(SingleDataReadRequest request, ServerCallContext context)
+    public override async Task<SingleDataReadResponse> GetSingleData(DataReadRequest request, ServerCallContext context)
     {
-        var response = _repo.LoadDataAsync(request.Collection, request.Key, request.Value);
+        Dictionary<string, string> searchParameters = request.Key.ToList().ToDictionary(key => key, key => request.Value.ToArray()[request.Key.ToList().IndexOf(key)]);
+        var response = _repo.LoadDataAsync(request.Collection, searchParameters);
         return await Task.FromResult(new SingleDataReadResponse
         {
             Value = response.First().ToString()
         });
     }
     
+    public override async Task<MultiDataReadResponse> GetMultiData(DataReadRequest request, ServerCallContext context)
+    {
+        Dictionary<string, string> searchParameters = request.Key.ToList().ToDictionary(key => key, key => request.Value.ToArray()[request.Key.ToList().IndexOf(key)]);
+        var response = _repo.LoadDataAsync(request.Collection, searchParameters);
+        return await Task.FromResult(new MultiDataReadResponse
+        {
+            Value = { response.Select(x => x.ToString()) }
+        });
+    }
     public override async Task<SingleDataWriteResponse> SetSingleData(SingleDataWriteRequest request, ServerCallContext context)
     {
         BsonDocument document = new();
